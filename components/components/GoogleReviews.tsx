c77692f0ecb7f90dd103b4234e792861653b0090
Lalit Rajput <rajput88lg@gmail.com>
2026-07-23 19:28:01 +0530

    Removed fake testimonials and added Google Reviews

--- components/components/GoogleReviews.tsx
+++ components/components/GoogleReviews.tsx
@@ -1,12 +1,14 @@
 export default function GoogleReviews() {
   return (
-    <section className="py-20 bg-white">
-      <div className="container mx-auto px-4">
-        <div
-          className="elfsight-app-7cae4e52-d20a-4fa9-b05a-3518b25b7660"
-          data-elfsight-app-lazy
-        ></div>
-      </div>
+    <section className="py-20 bg-red-500">
+      <h1 className="text-white text-5xl text-center">
+        Google Reviews Test
+      </h1>
+
+      <div
+        className="elfsight-app-7cae4e52-d20a-4fa9-b05a-3518b25b7660"
+        data-elfsight-app-lazy
+      />
     </section>
   );
 }
